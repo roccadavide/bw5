bffb99e669d32cf0913a4be4284bdb5192b8e7f8
ZairaStra <straticozaira@gmail.com>
2025-07-08 16:56:46 +0200

    PuntiVenditaDAO testati

--- src/main/java/daviderocca/Application.java
+++ src/main/java/daviderocca/Application.java
@@ -1,8 +1,14 @@
 package daviderocca;
 
+import daviderocca.dao.PuntiVenditaDAO;
+import daviderocca.entities.PuntoVendita;
+import daviderocca.entities.enums.StatoPuntoVendita;
+import daviderocca.entities.enums.TipologiaVendita;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
+import java.util.UUID;
 
 public class Application {
 
@@ -10,6 +16,9 @@ public class Application {
     public static void main(String[] args) {
 
         EntityManager em = emf.createEntityManager();
+        PuntiVenditaDAO pd= new PuntiVenditaDAO(em);
+
+       
 
         em.close();
         emf.close();
